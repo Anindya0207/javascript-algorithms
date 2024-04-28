f4302e757a22a00c68f3fa357437545bc1d6ca1b
anindya.sm <anindya.sm@flipkart.com>
2024-04-28 18:53:31 +0530

    graph theory

---
+++ src/playground/DSA class/27Apr/27Apr.md
@@ -0,0 +1,61 @@
+## GRAPH
+
+- Graph is mainly same as Tree. Only difference is Graph can have a cycle unlike Tree
+- The starting node for Graph is root
+
+
+## Graph types
+
+- Null graph: No edges in the graph
+- Trivial Graph: single vertex, smallest graph
+- Undirected graph: edges have no direction, Nodes/vertex are unordered pairs in the definition of every edge
+- Directed graph: edges have direction, Nodes are ordered pair in the definition of edges
+- Connected graph: one node can be visited from any other node in the graph, means there is no disconnection b/w any two nodes
+- Disconnected graph: there can be disconnection bw any two nodes in the graph;
+- Regular graph: If in a graph degree of every vertex is K, it's called K regular graph (TBD)
+- Complete graph: For each node there is a edge to all other nodes
+- Cycle graph: The graph is a cycle in itself, every vertex has degree as 2
+- Cyclic graph: A graph containing at least one cycle is a cyclic graph
+- Acyclic graph: A graph where there is no cycle
+- Bipartite graph: if there are two sets of nodes, each set doesn't connect within itselves
+- Weighted graph: pta ni kya h
+
+
+![alt text](image-3.png)
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-4.png)
+![alt text](image-5.png)
+
+
+## Graph represetation
+
+### Adj Matrix (2D array) O(n^2) complexity
+       V1 V2 V3 V4
+    V1 0   
+    V2
+    V3
+    V4
+    - Adj matrix representation is o(1) while inserting or deleting an edge but initialisation is o(n^2)
+
+Scarse graph/Null graph - no of edges less (If no of edges are less, lot of places will be 0, which will be a disadvantage)
+Dense graph - no of edges more
+
+
+### Adj List 
+
+    V1 -> V2, V3
+    V2 -> V1, V3
+    - insert of a edge is o(1) deletion of a edge is o(n) 
+    - initialisation is o(n)
+No disadvantage if there is a connection there wil be a node available
+
+
+# In degree out degree of a node
+
+In degree is the number of incoming edges to a node of a graph.
+In a tree DS, indegree of any node is 1 becuse there is only one edge incoming to any node in a tree.
+But in Graph, there can be n edges inoming to a node. Then in degree of the node is n. 
+
+Similarly, the number of edges outgoing from a node in a graph is called outdegree. In a BST, outdegree is <=2 
--- src/playground/playground.js
+++ src/playground/playground.js
@@ -17,241 +17,26 @@ const arrayLength =20000;
 
 let randomArray = generateUniqueRandomArray(arrayLength, minNumber, maxNumber);
 
-
-class Node {
-  constructor(_data, _left, _right, _height, _bf) {
-    this.data = _data;
-    this.left = _left;
-    this.right = _right;
-    this.height = _height;
-    this.bf = _bf;
+const playground = () => {
+  const arr = [
+    [0,1,1,1,0],
+    [1,0,0,0,1],
+    [1,0,0,1,0],
+    [1,0,1,0,1],
+    [0,1,0,1,0]
+  ];
+  console.log(arr);
+ let map = {}, edge = 0;
+  for(var i = 0; i < arr.length; i ++ ){ 
+    for(j = 0; j < arr[i].length; j++) {
+      if(arr[i][j] == 1 &&  !map[`${i}${j}`]) {
+        edge++;
+        map[`${i}${j}`] = true;
+        map[`${j}${i}`] = true;
+      }
+    }
   }
+  console.log(edge);
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-    this.createNode = (data) => {
-      const node = new Node(data, null, null, 1, 0);
-      return node;
-    }
-  }
-  insertWithLoop = (data) => {
-    const node = this.createNode(data);
-    if(this.root == null) {
-      this.root = node;
-    } else {
-      let pivot = this.root;
-      while (pivot != null) {
-        if(data > pivot.data) {
-          if(!pivot.right) {
-            pivot.right = node; break;
-          } else {
-            pivot = pivot.right;
-          }
-        } else {
-          if(!pivot.left) {
-            pivot.left = node; break;
-          } else {
-            pivot = pivot.left;
-          }
-        }
-      }
-     
-    }
-  }
-  insert = (data) => {
-    const node = this.createNode(data);
-    const _insert = (pivot) => {
-      if(!pivot) {
-        return node;
-      }
-      if(data > pivot.data) {
-        pivot.right = _insert(pivot.right);
-      } else if(data < pivot.data) {
-        pivot.left = _insert(pivot.left);
-      }
-      pivot.height = Math.max(pivot.left?.height || 0, pivot.right?.height || 0) + 1;
-      pivot.bf = (pivot.left?.height || 0) - (pivot.right?.height || 0);
-      return pivot;
-    }
-    if(this.root == null) {
-      this.root = node;
-    } else {
-      _insert(this.root);
-    }
-  }
-  displayInorder = () => {
-    const _display = (pivot) => {
-      if(!pivot) {
-        return;
-      }
-      _display(pivot.left);
-      console.log(pivot.data);
-      _display(pivot.right);
-    }
-    if(this.root == null) {
-      console.log("Tree is empty bye bye");
-      return;
-    } else {
-      _display(this.root);
-    }
-  }
-  displayInorderDesc = () => {
-    const _display = (pivot) => {
-      if(!pivot) {
-        return;
-      }
-      _display(pivot.right);
-      console.log(pivot.data);
-      _display(pivot.left);
-    }
-    if(this.root == null) {
-      console.log("Tree is empty bye bye");
-      return;
-    } else {
-      _display(this.root);
-    }
-  }
-  displayPreorder = () => {
-    const _display = (pivot) => {
-      if(!pivot) {
-        return;
-      }
-      console.log(pivot.data);
-      _display(pivot.left);
-      _display(pivot.right);
-    }
-    if(this.root == null) {
-      console.log("Tree is empty bye bye");
-      return;
-    } else {
-      _display(this.root);
-    }
-  }
-  displayPostOrder = () => {
-    const _display = (pivot) => {
-      if(!pivot) {
-        return;
-      }
-      _display(pivot.left);
-      _display(pivot.right);
-      console.log(pivot.data);
-    }
-    if(this.root == null) {
-      console.log("Tree is empty bye bye");
-      return;
-    } else {
-      _display(this.root);
-    }
-  }
-  search = (data) => {
-    const _search = (pivot) => {
-      if(!pivot) return null;
-      if(pivot.data == data) {
-        return pivot;
-      }
-      if(data > pivot.data) {
-        return _search(pivot.right);
-      } else if(data < pivot.data) {
-        return _search(pivot.left);
-      }
-      return null;
-    }
-    if(this.root == null) {
-      console.log("Tree is empty. bye bye")
-    } else {
-      console.log(_search(this.root));
-    }
-  }
-  delete = (data) => {
-    const _find = (pivot) => {
-      if(!pivot.left) return pivot;
-      return _find(pivot.left);
-    }
-    const _deleteDuplicate = (pivot, element) => {
-      if(pivot.left == element) {
-        pivot.left = element.right;
-        return pivot;
-      }
-      return _deleteDuplicate(pivot.left, element);
-    }
-    const _delete = (pivot) => {
-      if(!pivot) return null;
-      if(pivot.data == data) {
-        if(!pivot.right && !pivot.left) {
-          return null;
-        }
-        if(pivot.right && !pivot.left) {
-          return pivot.right;
-        }
-        if(pivot.left && !pivot.right) {
-          return pivot.left;
-        }
-        if(pivot.left && pivot.right) {
-          let _currentLeft = pivot.left;
-          let _currentRight = pivot.right;
-          let _leftMostChild = _find(pivot.right);
-          if(_leftMostChild != _currentRight) {
-            _currentRight = _deleteDuplicate(_currentRight, _leftMostChild);
-            _leftMostChild.right = _currentRight;
-          }
-          _leftMostChild.left = _currentLeft;
-          return _leftMostChild;
-        }
-      }
-      if(data > pivot.data) {
-        pivot.right = _delete(pivot.right);
-      }
-      else if(data < pivot.data) {
-        pivot.left = _delete(pivot.left);
-      }
-      return pivot;
-    };
-    if(this.root == null) {
-      console.log("Tree is empty. bye bye")
-      return;
-    }
-    this.root = _delete(this.root);
-    console.log(this.root);
-  }
-  findClosest = (data) => {
-    const map = {}, minDiff = 9999999999;
-    const _find = (pivot) => {
-      if(!pivot) {
-        return map[minDiff];
-      }
-      if(pivot.data == data) {
-        return pivot;
-      } else {
-        diff = Math.abs(pivot.data - data);
-        minDiff = Math.min(diff, minDiff);
-        map[diff] = pivot;
-        if(data > pivot.data) {
-          return _find(pivot.right);
-        } else if(data < pivot.data) {
-          return _find(pivot.left);
-        }
-      }
-    }
-    if(this.root == null) {
-      return null;
-    } else {
-      _find(this.root);
-    }
-  }
-} 
-
-const bst = new BinarySearchTree();
-
-bst.insert(20);
-bst.insert(64);
-bst.insert(58);
-bst.insert(89);
-bst.insert(8);
-bst.insert(91);
-bst.insert(11);
-bst.insert(70);
-bst.insert(71);
-
-console.log(bst);
+playground();

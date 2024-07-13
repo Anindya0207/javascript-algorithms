56bef9341411c57b6a3efba9e8e82069b53966c7
anindya.sm <anindya.sm@flipkart.com>
2024-07-13 15:39:09 +0530

    done

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -1,33 +1,16 @@
-var candy = function (ratings) {
-    let i = 0, j = i+1, total = 1, peak;
-    while(j < ratings.length) {
-        debugger
-        if(ratings[j] != undefined && ratings[j] == ratings[i]) {
-            total += 1;
-            i++;
-            j++;
-            continue;
+var eraseOverlapIntervals = function(final) {
+    final.sort((a, b) => a[1] - b[1]);
+    let count = 1;
+    let j = 1,
+        k = j - 1;
+    while (j < final.length) {
+        if (final[j] && final[j][0] >= final[k][1]) {
+            count++;
+            k = j;
         }
-        peak = 1
-        while(ratings[j] != undefined && ratings[j] > ratings[i]) {
-            peak++;
-            total += peak;
-            i++;
-            j++;
-        }
-        down = 1; 
-        while(ratings[j] != undefined && ratings[j] < ratings[i]) {
-            total += down;
-            down++;
-            i++;
-            j++;
-        }
-        if(down > peak) {
-            total = total + (down - peak);
-        }
+        j++
     }
-    return total;
+    return final.length - count;
 };
 
-// console.log(candy([0, 2, 4, 7, 6, 5, 4, 3,2, 1,1,1,2,3,4, 2,1,1,1]))
-console.log(candy([1,0,2]))
+console.log(eraseOverlapIntervals([[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]]))

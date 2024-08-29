664715eea9f8858238d895a8793e10058d4d3621
Anindya Mukherjee <anindya.sanchi@Anindyas-MacBook-Pro.local>
2024-08-29 20:56:34 +0530

    dsgsdg

--- src/playground/Strivers/DPTheories.md
+++ src/playground/Strivers/DPTheories.md
@@ -57,3 +57,35 @@ bottomUp(n) {
 }
 ```
 --------------------------------------------------------------------------------------------------------------------------------
+
+### Tricks to solve DP problems
+
+- Step1: See what is asked in the problem.  it can be finding min or max or count. Whatever is asked for dp[i] should hold exactly that. 
+ie. for example we want to find the minimum cost to reaach n then dp[i] = mimn cost to reach i
+- Step2: figure out the recurring statement, most of the time it will be like ith step can be accessed from i-1 or i-2 or i-k. So from i-k to i whatever constraint it's given plus dp[i-k] will give us dp[i]
+- Step3: Set the initial values like dp[0] and dp[1] 
+
+e.g: You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+- It's said to return the max profit, so we will keep dp[i] = max robbed profit till ith index. 
+- Now, we couldn't have robbed i-1 when we are robbing i, right? So we might have got some profit at i-1 which is dp[i-1] but we can't add the ith profit with that. But we can add the ith profit with i-2th profit, right? why? because I can rob houses which are not adjacent.
+- So can i say? dp[i] is maximum of 
+    - whatever max profit I got till i-1 and
+    - whatever max profit I got till i-2 + profit at ith house
+- Hence `dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);`
+- Now for initialisation what profit I will get at 0th house? it's nums[0], right?
+- And whats the max profit at 1st house? it's either 0th house ka profit or 1st house ka. we can;t add them up as they are adjacent. So dp[1] = Math,max(nums[0], nums[1]);
+
+```javascript
+ var rob = function(nums) {
+    let dp = Array.from({length: nums.length}, () => -1);
+    dp[0]= nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+    }
+    return dp[nums.length -1];
+};
+```
+
+--------------------------------------------------------------------------------------------------------------------------------  
--- src/playground/playground.js
+++ src/playground/playground.js
@@ -1,61 +1,15 @@
-/**
- * @param {number[][]} stones
- * @return {number}
- */
-function DisjoinSet(size) {
-  this.rankArr = Array.from({length: size + 1} , () => 0);
-  this.sizeArr = Array.from({length: size + 1} , () => 1);
-  this.parentArr = Array.from({length: size + 1});
-  for(let i  = 0; i <= size; i++) {
-    this.parentArr[i] = i;
+const minimizeCost = (arr, k) => {
+  let n = arr.length;
+  let dp = Array.from({length: n}, () => -1);
+  dp[0] = 0;
+  dp[1] = Math.abs(arr[1] - arr[0]);
+  for(let i = 2; i < n; i++) {
+      let min = Infinity;
+      for(let j = i-1; j >= Math.max(i - k, 0); j--) {
+          min = Math.min(min, dp[j] + Math.abs(arr[j] - arr[i]));
+      }
+      dp[i] = min;
   }
+  return dp[n-1]
 }
-
-DisjoinSet.prototype.findParent = function(pivot) {
-  if(this.parentArr[pivot] == pivot) return pivot;
-  this.parentArr[pivot] = this.findParent(this.parentArr[pivot]);
-  return this.parentArr[pivot];
-}
-
-DisjoinSet.prototype.unionRank = function(U, V){ 
-  let parentU = this.findParent(U);
-  let parentV = this.findParent(V);
-  if(parentU == parentV) return;
-  if(this.rankArr[parentU] > this.rankArr[parentV]) {
-    this.parentArr[parentV] = parentU;
-  } else if(this.rankArr[parentU] < this.rankArr[parentV] ) {
-    this.parentArr[parentU] = parentV;
-  } else {
-    this.parentArr[parentV] = parentU
-    this.rankArr[parentU]++;
-  }
-}
-var removeStones = function(stones) {
-    let maxR = -Infinity;
-    let maxC = -Infinity;
-    let uniqueParent = {};
-    for(let i =0; i< stones.length;i++) {
-        let [u,v] = stones[i];
-        maxR = Math.max(maxR, u);
-        maxC = Math.max(maxC, v);
-    }
-    let dj = new DisjoinSet(maxR + maxC + 1);
-    for(let i = 0; i < stones.length; i++) {
-        let [u,v] = stones[i];
-        let normalisedCol = v + maxR + 1
-        dj.unionRank(u, normalisedCol);
-    }
-    for(let i = 0; i < stones.length; i++) {
-        let [u,v] = stones[i];
-        let normalisedCol = v + maxR + 1
-        let parentU = dj.findParent(u);
-        let parentV = dj.findParent(normalisedCol);
-        uniqueParent[parentU]++
-        uniqueParent[parentV]++
-    }
-    let totalDisConnected = Object.keys(uniqueParent).length
-    return stones.length - totalDisConnected
-};
-
-
-console.log(removeStones([[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]))
+  console.log(minimizeCost( [10, 30, 40, 50, 20], 3))

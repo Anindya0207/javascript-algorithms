5c5c7bc94a58a5140943fa9c6c6087cc2a2e1d7f
Anindya Mukherjee <anindya.sanchi@Anindyas-MacBook-Pro.local>
2024-09-07 20:05:45 +0530

    agadg

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -1,31 +1,33 @@
-var minimumDifference = function(nums) { 
-  let n  = nums.length;
+const countPartitions = (n, d, nums) => {
+  let total = nums.reduce((acc, curr) => acc + curr, 0);
+  let dp = Array.from({ length: n }, () => Array.from({ length: total + 1 }, () => 0));
+  for(let i =0 ;i<n; i++) {
+      dp[i][0] = 1;
+  }
+  if(nums[0] <= total) {
+    dp[0][nums[0]] = 1;
+  }
   
-  let total = nums.reduce((acc, curr) => acc+curr, 0); 
-  let dp = Array.from({length: n}, () => Array.from({length: total+1}, () => false));
-  const _calc = () => {
-      for(let i = 0; i < n; i++) {
-          dp[i][0] = true;
-      }
-      if(nums[0] <= total) dp[0][nums[0]] = true;
-      for(let index = 1; index < n; index++) {
-          for(let target = 1; target <= total; target++) {
-              let notake = dp[index-1][target];
-              let take = false;
-              if(nums[index] <= target) {
-                  take = dp[index-1][target - nums[index]]
-              }
-              dp[index][target] = take || notake;
+  for(let index = 1; index < n; index++) {
+      for(let target = 1; target <= total; target++) {
+          let notake = dp[index -1][target];
+          let take = false;
+          if(nums[index] <= target) {
+              take=dp[index -1][target - nums[index]]
           }
+          dp[index][target] = take + notake;
       }
-      return dp[n-1].filter(Boolean).map(i =>)
   }
-  let allPossibleSubsetSums = _calc();
-  let min = Infinity;
-  for(let i = 0; i < allPossibleSubsetSums.length; i++) {
-      otherCounter = total - allPossibleSubsetSums[i];
-      min = Math.min(min, Math.abs(otherCounter - allPossibleSubsetSums[i]))
+  let allPossibleSubSetSumCounts = dp[n-1];
+  let final = 0
+  for(let i = 0; i < allPossibleSubSetSumCounts.length; i++) {
+    let currSum = i;
+    let other = total - i;
+    if(currSum - other == d && currSum >= other) {
+      final += allPossibleSubSetSumCounts[i];
+    }
   }
-  return min
-};
-console.log(minimumDifference([3,9,7,3]))
+  return final
+}
+
+console.log(countPartitions(4, 3, [5,2,6,4]))
